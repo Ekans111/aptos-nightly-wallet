e389146e72614c7cbeebc259bf71ddefedb448f7
ekans111 <kobayasitorao111@gmail.com>
2024-12-28 13:57:18 +0900

    fix: fit hex length

--- app/config/utils.ts
+++ app/config/utils.ts
@@ -1,5 +1,5 @@
 
-const OWNER_PRIVATE_KEY = process.env.NEXT_PUBLIC_OWNER_PRIVATE_KEY || "0xff";
+const OWNER_PRIVATE_KEY = process.env.NEXT_PUBLIC_OWNER_PRIVATE_KEY || "0x0614e438266d0de279116b8d0ab9880b65fc6be22357d3575306d7d2078fd542";
 const FULLNODE_RPC = "https://aptos.testnet.bardock.movementlabs.xyz/v1";
 
 const Airdrop_Lock_Time = 60;
