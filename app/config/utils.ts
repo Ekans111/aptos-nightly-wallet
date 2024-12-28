8bbc6aeda88ca79d53a0029cfc139272b1286067
ekans111 <kobayasitorao111@gmail.com>
2024-12-28 13:49:16 +0900

    fix: update hex length

--- app/config/utils.ts
+++ app/config/utils.ts
@@ -1,5 +1,5 @@
 
-const OWNER_PRIVATE_KEY = process.env.NEXT_PUBLIC_OWNER_PRIVATE_KEY || "0x";
+const OWNER_PRIVATE_KEY = process.env.NEXT_PUBLIC_OWNER_PRIVATE_KEY || "0x0000000000000000000000000000000000000000000000000000000000000000";
 const FULLNODE_RPC = "https://aptos.testnet.porto.movementlabs.xyz/v1";
 
 const Airdrop_Lock_Time = 60;
