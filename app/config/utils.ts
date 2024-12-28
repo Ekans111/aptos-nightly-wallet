638508bbd9421385730e930fa32eaf42056f2342
ekans111 <kobayasitorao111@gmail.com>
2024-12-28 13:45:59 +0900

    fix: hex string fit

--- app/config/utils.ts
+++ app/config/utils.ts
@@ -1,5 +1,5 @@
 
-const OWNER_PRIVATE_KEY = process.env.NEXT_PUBLIC_OWNER_PRIVATE_KEY || "0x000000000";
+const OWNER_PRIVATE_KEY = process.env.NEXT_PUBLIC_OWNER_PRIVATE_KEY || "0x";
 const FULLNODE_RPC = "https://aptos.testnet.porto.movementlabs.xyz/v1";
 
 const Airdrop_Lock_Time = 60;
